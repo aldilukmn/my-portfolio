9216b0d458711d3a3b3f73737c91faad5c888eb6
Aldi Lukman Maulana Latief <aldilukman99@gmail.com>
2023-06-19 23:58:34 +0700

    jumbotron

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
-$('#actionNav').click(function() {
-    $('.navigation').toggleClass('hideNav showNav');
+$('#actionNav').click(function(e) {
+    e.stopPropagation();
+    $('.navigation').toggleClass('hideNav');
+});
+
+$(document).click(function() {
+    if ($('.navigation').hasClass('hideNav')) {
+        $('.navigation').removeClass('hideNav');
+        $('#actionNav').prop('checked', false);
+    }
 });
